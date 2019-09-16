1ed448d3e7526e077dca8a39da429c584e7cc6d5
Daniel Bradbury <danieljamesbradbury@hotmail.com>
2019-09-16 20:18:20 +0100

    Update depend.sh

--- code/depend.sh
+++ code/depend.sh
@@ -3,7 +3,7 @@ sudo apt install rustc -y
 sudo apt install ruby -y
 sudo apt install python3 -y
 sudo apt install php -y
-sudo apt install lua5.3 -y
+sudo apt install lua5.2 -y
 sudo apt install r-base -y
 sudo apt install nodejs -y
 sudo apt install wine-stable -y
